c906d7d6900a87b11c24d521b870ce9ae32b2745
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2023-08-30 19:19:42 -0400

    Implementing get bet funtion for get the bet amount

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 
 def deposit():
@@ -31,9 +33,23 @@ def get_number_of_lines():
     return lines
 
 
+def get_bet():
+    while True:
+        amount = input("What would you like to bet? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between {MIN_BET} - {MAX_BET}. ")
+        else:
+            print("Please enter a number.")
+
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
+    bet = get_bet()
     print(balance, lines)
 
 
