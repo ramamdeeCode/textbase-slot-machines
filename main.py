af814b84989a89a224ff31d1aede274567589bf2
Rama <86864383+ramamdeeCode@users.noreply.github.com>
2023-09-01 23:58:09 -0400

    adding machine spinning and printint function

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
+import random
+
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8,
+}
+
+
+def get_clot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(all_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        columns.append(column)
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|", end=" ")
+            else:
+                print(column[row])
 
 
 def deposit():
@@ -35,7 +75,7 @@ def get_number_of_lines():
 
 def get_bet():
     while True:
-        amount = input("What would you like to bet? $")
+        amount = input("What would you like to bet on each line? $")
         if amount.isdigit():
             amount = int(amount)
             if MIN_BET <= amount <= MAX_BET:
@@ -44,13 +84,26 @@ def get_bet():
                 print(f"Amount must be between {MIN_BET} - {MAX_BET}. ")
         else:
             print("Please enter a number.")
+    return amount
 
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    print(balance, lines)
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if total_bet > balance:
+            print(
+                f"You do not have enough to bet that amount, your current balance is ${balance}"
+            )
+        else:
+            break
+    print(
+        f"You are betting ${bet} on {lines} lines. Total bet is equal to : ${total_bet}"
+    )
+    slots = get_clot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
